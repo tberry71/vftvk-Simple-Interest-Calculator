5be8e5b98e85ab3bf746cfa26370e27df31bb4b5
tberry71 <berrytanesha71gmail.com>
2022-01-30 03:15:56 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
-
-    if(principal == "" || principal <= 0)
-    {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-        return;
-    }
-
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-
-    var dateNow = new Date();
-    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
-    
-    var resultDisplay = document.getElementById("result");
-    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
 }
 
-function SliderValue()
+function updateRate() 
 {
-    var slider = document.getElementById("rate");
-    var output = document.getElementById("rate_display");
-    output.innerHTML = slider.value; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() 
-    {
-        output.innerHTML = this.value;
-    }  
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
